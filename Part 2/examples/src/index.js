94594cf0415c84b401a1a81ceffe3a2cce4e688f
Alex <15ambm@queensu.ca>
2020-06-04 12:33:12 -0400

    - componentized Note.js and App.js

--- Part 2/examples/src/index.js
+++ Part 2/examples/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from "./App";
 
 const notes = [
   {
@@ -21,26 +22,6 @@ const notes = [
     important: true
   }
 ]
-
-const Note = ({note}) => {
-  return (
-    <li>{note.content}</li>
-  )
-}
-
-const App = ({notes}) => {
-  
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note}/>
-          )}
-      </ul>
-    </div>
-  )
-}
 
 ReactDOM.render(
   <App notes={notes} />,
