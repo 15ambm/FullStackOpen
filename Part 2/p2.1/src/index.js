b3e6536542212218acc4b7f60a03909c15e536d7
Alex <15ambm@queensu.ca>
2020-06-04 13:20:38 -0400

    - completed 2.4

--- Part 2/p2.1/src/index.js
+++ Part 2/p2.1/src/index.js
@@ -45,34 +45,54 @@ const Total = ({ course }) => {
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'A new part',
-        exercises: 2
-      },
-      {
-        name:'Redux',
-        exercises: 9
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        },
+        {
+          name: 'A new part',
+          exercises: 2
+        },
+        {
+          name:'Redux',
+          exercises: 9
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+]
 
   return (
-    <Course course={course}/>
+    <div>
+    {courses.map(course => <Course key={course.name} course={course}/>)}
+    </div>
   )
 }
 
