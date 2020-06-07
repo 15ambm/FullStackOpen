ef1f440314fd586386cfdb5e2616e904e6775446
Alex <15ambm@queensu.ca>
2020-06-07 15:45:23 -0400

    - part 2.2 example, add toggle button for list

--- Part 2/examples/src/App.js
+++ Part 2/examples/src/App.js
@@ -4,9 +4,8 @@ import Note from "./components/Note";
 const App = (props) => {
 
     const [notes, setNotes] = useState(props.notes)
-    const [newNote, setNewNote] = useState(
-      'A new note...'
-    )
+    const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(true)
   
     const addNote = (event) => {
       event.preventDefault()
@@ -20,17 +19,23 @@ const App = (props) => {
       setNewNote('')
     }
 
-
     const handleNoteChange = (event) => {
       console.log(event.target.value)
       setNewNote(event.target.value)
     }
 
+    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
     return (
       <div>
         <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? "important" : "all"}
+          </button>
+        </div>
         <ul>
-          {notes.map(note => 
+          {notesToShow.map(note => 
             <Note key={note.id} note={note}/>
             )}
         </ul>
