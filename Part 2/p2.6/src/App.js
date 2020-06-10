94e4685d4319c678f43cbd593221ccfecd0f5235
Alex <15ambm@queensu.ca>
2020-06-10 13:31:35 -0400

    - 2.8

--- Part 2/p2.6/src/App.js
+++ Part 2/p2.6/src/App.js
@@ -3,9 +3,11 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas',
+      number: "39-54",
       id: 1 }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
 
   // KEY TAKEAWAY: Passing the event parameter to our handler
@@ -14,18 +16,25 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNewNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   // Don't forget to prevent default 
-  const addNewName = (event) => {
+  const addNewPerson = (event) => {
     event.preventDefault()
     if(persons.find(person => person.name === newName)) {
       window.alert(`${newName} is already in the phonebook`)
     } else {
       const tempNewName = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1
       }
       setPersons(persons.concat(tempNewName))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -37,11 +46,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNewNameChange}/>
         </div>
         <div>
-          <button type="submit" onClick={addNewName}>add</button>
+          number: <input value={newNumber} onChange={handleNewNumberChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addNewPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <li key={person.id}>{person.name}</li>)}
+  {persons.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
     </div>
   )
 }
