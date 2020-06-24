07cb31e6c1e5eac4c142cd227ce5b6a0de2eb7ad
Alex <15ambm@queensu.ca>
2020-06-24 16:58:57 -0400

    - 2.11 connecting to json server for notes

--- Part 2/p2.6/src/App.js
+++ Part 2/p2.6/src/App.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonForm from "./components/PersonForm";
 import Persons from './components/Persons';
 import Filter from './components/Filter';
+import axios from "axios";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-      { name: 'Arto Hellas', number: "39-54", id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523',id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345',id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ showAll, toggleShowAll] = useState(true)
   const [ filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('persons',persons.length);
 
   // KEY TAKEAWAY: Passing the event parameter to our handler
   const handleNewNameChange = (event) => {
