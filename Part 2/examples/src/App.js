5ca21dbf452fb536c247a39694e1d295d4148b45
Alex <15ambm@queensu.ca>
2020-07-08 13:04:47 -0400

    - moved interaction with the server into its own module - added the ability to toggle the importance of notes and update the server - introduced some basic promise error handling

--- Part 2/examples/src/App.js
+++ Part 2/examples/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Note from "./components/Note";
 import axios from 'axios';
+import noteService from "./services/notes";
 
 const App = (props) => {
 
@@ -9,15 +10,16 @@ const App = (props) => {
     const [showAll, setShowAll] = useState(true)
   
     useEffect(() => {
-      console.log("effect")
-      axios
-        .get('http://localhost:3001/notes')
-        .then(response => {
-          console.log('response ');
-          setNotes(response.data)
+      noteService
+        .getAll()
+        .then(initialResponse => {
+          setNotes(initialResponse)
+        })
+        .catch(error => {
+          alert(`there has been a problem`)
         })
     }, [])
-    console.log('render', notes.length, 'notes')
+
 
     const addNote = (event) => {
       event.preventDefault()
@@ -27,13 +29,39 @@ const App = (props) => {
         important: Math.random() < 0.5,
         id: notes.length + 1
       }
-      setNotes(notes.concat(noteObject))
-      setNewNote('')
+
+      noteService
+        .create(noteObject)
+        .then(response =>{
+           setNotes(notes.concat(response))
+           setNewNote('')
+          })
     }
 
     const handleNoteChange = (event) => {
       console.log(event.target.value)
       setNewNote(event.target.value)
+    }
+    
+    const toggleNoteImportance = (id) => {
+      console.log("toggle id ", id)
+      
+      const url = `http://localhost:3001/notes/${id}`
+      const note = notes.find(n => n.id === id)
+      const changedNote = {...note, important: !note.important}
+
+      noteService
+        .update(id, changedNote)
+        .then(response => {
+          console.log(response)
+          setNotes(notes.map(note => note.id !== id ? note : response))
+        })
+        .catch(error => {
+          alert(`there has been a problem`)
+          setNotes(notes.filter(n => id !== n.id))
+        })
+
+
     }
 
     const notesToShow = showAll ? notes : notes.filter(note => note.important)
@@ -48,7 +76,7 @@ const App = (props) => {
         </div>
         <ul>
           {notesToShow.map(note => 
-            <Note key={note.id} note={note}/>
+            <Note key={note.id} note={note} toggleImportance={() => toggleNoteImportance(note.id)}/>
             )}
         </ul>
         <form onSubmit={addNote}>
